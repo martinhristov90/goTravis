84ffb91145b3c33f9e965d13fdb16cc48b6e24d4
Martin Hristov <martinhristov@Martins-MBP.lan>
2019-04-30 15:52:46 +0300

    Adding TravisCI

---
+++ travisTest.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This line executes the script that needs to be tests and stores the out in the# variable "output"
+
+go build hello.go
+output=$(./hello)
+
+# we compare output to "hello"
+if [ "${output}" == "hello" ]; then
+  # if output match all good
+  echo GOOD: Test Pass
+  exit 0
+else
+  # if output doesn't match - mr dragon is not happy
+  echo BAD: Test fail
+  exit 1
+fi 
